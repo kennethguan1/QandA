72702ef82681b382b115c65c747cfca1e1f28001
kennethguan1 <kennethguan1@gmail.com>
2020-11-03 08:25:31 -0800

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
 
-User.destroy_all
-Question.destroy_all
 Comment.destroy_all
+Question.destroy_all
+User.destroy_all
 
 demoUser = User.create(username: "demo_user", email: "demo@demo.com", password: "123456")
 happyUser = User.create(username: "happy_user", email: "happy@user.com", password: "654321")
 curiousUser = User.create(username: "curious_user", email: "curious@user.com", password: "121212")
 
-Question.create(title: "Which came first? Chicken or the egg?", body: "Or maybe ducks.", author_id: "1")
-Question.create(title: "Where can I learn more about plexy glass?", body: "I already tried the library.", author_id: "2")
-Question.create(title: "Are raisins safe to eat?", body: "My friend's uncle's nanny told me about toxins.", author_id: "3")
+question1 = Question.create(title: "Which came first? Chicken or the egg?", body: "Or maybe ducks.", author_id: demoUser.id)
+question2 = Question.create(title: "Where can I learn more about plexy glass?", body: "I already tried the library.", author_id: happyUser.id)
+question3 = Question.create(title: "Are raisins safe to eat?", body: "My friend's uncle's nanny told me about toxins.", author_id: curiousUser.id)
 
-Comment.create(body: "Chicken because C comes before D and E", author_id: "2", question_id: "1")
+comment1 = Comment.create(body: "Chicken because C comes before D and E", author_id: demoUser.id, question_id: question1.id)
+
+end
